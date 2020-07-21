efc5a38a5d42830b125fc143fd380790849c6185
Deivids Agriņš <Deivids.Agrins@edu.rtu.lv>
2020-07-21 20:26:50 +0300

    initial Dependency injection and created business logic

---
+++ AspNetApiControllers/AspNetApiControllers/Controllers/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace AspNetApiControllers.Controllers
+{
+    public interface IPersonService
+    {
+        Person GetPersonById(int id);
+        List<Person> GetAllPeople();
+        void AddPerson(Person person);
+        Person UpdatePerson(Person person);
+        Person DeletePersonById(int id);
+    }
+}
---
+++ AspNetApiControllers/AspNetApiControllers/Controllers/PersonController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetApiControllers.Controllers
+{
+    [ApiController]
+    [Route("api/{controller}")]
+    public class PersonController : ControllerBase
+    {
+
+        private IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public List<Person> Get()
+        {
+            return _personService.GetAllPeople();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Person> Get(int id)
+        {
+            var person = _personService.GetPersonById(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return person;
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Person> Delete(int id)
+        {
+            
+            var personToDelete = _personService.DeletePersonById(id);
+
+            if (personToDelete == null)
+            {
+                return BadRequest();
+            }
+
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult<Person> Add(Person person)
+        {
+            _personService.AddPerson(person);
+            return person;
+        }
+
+        [HttpPut]
+        public ActionResult<Person> Update(Person person)
+        {
+
+            var personToUpdate = _personService.UpdatePerson(person);
+            if (personToUpdate == null)
+            {
+                return BadRequest();
+            }
+
+            return personToUpdate;
+        }
+    }
+}
