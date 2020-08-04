0cd9935471ce05476094b40e52a9f40309a8bd0e
Deivids Agriņš <Deivids.Agrins@edu.rtu.lv>
2020-08-04 20:22:47 +0300

    Added person UI controller, js, css, layout

---
+++ AspNetApiControllers/AspNetApiControllers/Controllers/PersonUiController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetApiControllers.Controllers
+{
+    public class PersonUiController : Controller
+    {
+        private IPersonService _personService;
+        public PersonUiController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+        public IActionResult Index()
+        {
+            var people = _personService.GetAllPeople();
+            return View(people);
+        }
+
+        public IActionResult GetPerson(int id)
+        {
+            var person = _personService.GetPersonById(id);
+            return View(person);
+        }
+        
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Person person)
+        {
+            _personService.AddPerson(person);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- AspNetApiControllers/AspNetApiControllers/Startup.cs
+++ AspNetApiControllers/AspNetApiControllers/Startup.cs
@@ -18,6 +18,7 @@ namespace AspNetApiControllers
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddMvc();
 
             services.AddSingleton<IPersonService, PersonService>();
         }
@@ -32,9 +33,15 @@ namespace AspNetApiControllers
 
             app.UseRouting();
 
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                   pattern: "{controller=Home}/{action=index}/{id?}");              
             });
         }
     }
